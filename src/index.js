ebfcb88d73f3c7755565844b2d449aeb2fb8f9fa
Le Huynh <lhuynh100@gmail.com>
2022-09-29 11:22:58 -0500

    commit message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
+
+
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // CONSTANTS    
+      const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+      const imgContainer = document.getElementById('dog-image-container');
+      const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+      const breedList = document.getElementById('dog-breeds');
+      const breedDropDown = document.getElementById('breed-dropdown');
+      let breeds = [];
+      breedDropDown.addEventListener('change', showOnly)
+  
+      let imgArray = fetch(imgUrl)
+          .then( res => res.json() )
+          .then(json => {
+              for (let i=0; i < json.message.length; i++){
+                      let img = document.createElement('img');
+                      img.src = json.message[i]
+                      imgContainer.appendChild(img);
+              }
+          })
+  
+      let breedArray = fetch(breedUrl)
+          .then( res => res.json() )
+          .then(json => { 
+              for (const breed in json.message){ breeds.push(breed); }
+              breeds.forEach( breed => displayBreedList(breed))
+          })
+  
+          function showOnly(e) {
+              filteredBreeds = []
+              while (breedList.hasChildNodes()) {
+                  breedList.removeChild(breedList.firstChild);
+              }
+              filteredBreeds.push(breeds.filter(breed => breed.charAt(0) == e.target.value))
+              filteredBreeds[0].forEach( breed => displayBreedList(breed))
+          }
+  
+          function changeColour(){
+              this.style.color = 'blue';
+          }
+  
+          function displayBreedList(breed) {
+              let breedItem = document.createElement('li');
+                  breedItem.innerText = breed;
+                  breedList.appendChild(breedItem);
+                  breedItem.addEventListener('click', changeColour)
+          }
+  });
+
